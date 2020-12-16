2a58ac791bb3c6bd135e153f9ba04342b7c31fa1
isasilva3 <55702420+isasilva3@users.noreply.github.com>
2020-12-16 18:07:10 +0000

    Prediction second alteration

--- SegEx.py
+++ SegEx.py
@@ -195,8 +195,9 @@ for epoch in range(num_epochs):
             bimages = bimages.to(device)
             bsegs = bsegs.to(device)
 
-            prediction = torch.nn.Sigmoid(prediction)
-            pred_metric = metric(torch.nn.Sigmoid(prediction), bsegs)
+            m = torch.nn.Sigmoid()
+            prediction = net(bimages)
+            pred_metric = metric(m(prediction), bsegs)
             metric_vals.append(pred_metric.item())
 
     epoch_metrics.append((total_step, np.average(metric_vals)))
