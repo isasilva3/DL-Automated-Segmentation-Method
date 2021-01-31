c0b059d2a386fe1c3b9546448a4ab952ccef2ed9
isasilva3 <55702420+isasilva3@users.noreply.github.com>
2021-01-31 01:36:35 +0000

    Data

--- LungSegmentation.py
+++ LungSegmentation.py
@@ -88,9 +88,9 @@ The dataset comes from http://medicaldecathlon.com/.
 
 #resource = "https://msd-for-monai.s3-us-west-2.amazonaws.com/Task09_Spleen.tar"
 md5 = "410d4a301da4e5b2f6f86ec3ddba524e"
-root_dir="\home\imoreira\data"
-compressed_file = os.path.join(root_dir, "GOLDSTANDARD.rar")
-data_dir = os.path.join(root_dir, "GOLDSTANDARD")
+root_dir="/home/imoreira/Data/"
+compressed_file = os.path.join(root_dir, "Lungs.rar")
+data_dir = os.path.join(root_dir, "Lungs")
 #if not os.path.exists(data_dir):
 #    download_and_extract(resource, compressed_file, root_dir, md5)
 
