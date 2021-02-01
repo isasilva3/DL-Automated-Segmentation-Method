8f178660a1bd90de996b42f20c56f5acac08f2fe
isasilva3 <55702420+isasilva3@users.noreply.github.com>
2021-02-01 13:57:31 +0000

    Dataset extraction

--- LungSegmentation.py
+++ LungSegmentation.py
@@ -23,6 +23,11 @@ Size: 10 3D volumes (8 Training + 2 Testing)
 Source: Catarina
 
 """
+import pathlib
+
+from numpy import extract
+
+from MONAI.monai.apps import extractall
 
 """## Setup imports"""
 
@@ -86,13 +91,18 @@ Downloads and extracts the dataset.
 The dataset comes from http://medicaldecathlon.com/.
 """
 
+#var = (r'C:\Users\isasi\OneDrive\Desktop\Lungs.gz')
 #resource = "https://msd-for-monai.s3-us-west-2.amazonaws.com/Task09_Spleen.tar"
 md5 = "410d4a301da4e5b2f6f86ec3ddba524e"
-root_dir="/home/imoreira/Data/"
-compressed_file = os.path.join(root_dir, "Lungs.rar")
+
+#filepath = r'C:\Users\isasi\OneDrive\Desktop\Lungs.zip'
+filepath = '/home/imoreira/Data/Lungs.zip'
+compressed_file = os.path.join(root_dir, "Lungs.zip")
 data_dir = os.path.join(root_dir, "Lungs")
-#if not os.path.exists(data_dir):
-#    download_and_extract(resource, compressed_file, root_dir, md5)
+if not os.path.exists(data_dir):
+    extractall(filepath, root_dir, hash_type='md5')
+
+
 
 """## Set MSD Spleen dataset path"""
 
@@ -102,7 +112,7 @@ data_dicts = [
     {"image": image_name, "label": label_name}
     for image_name, label_name in zip(train_images, train_labels)
 ]
-train_files, val_files = data_dicts[:-9], data_dicts[-9:]
+train_files, val_files = data_dicts[:-9], data_dicts[-9:] #HERE
 
 """## Set deterministic training for reproducibility"""
 
@@ -168,7 +178,7 @@ val_transforms = Compose(
 check_ds = Dataset(data=val_files, transform=val_transforms)
 check_loader = DataLoader(check_ds, batch_size=1)
 check_data = first(check_loader)
-image, label = (check_data["image"][0][0], check_data["label"][0][0])
+image, label = (check_data["image"][0][0], check_data["label"][0][0]) #HERE
 print(f"image shape: {image.shape}, label shape: {label.shape}")
 # plot the slice [:, :, 80]
 plt.figure("check", (12, 6))
