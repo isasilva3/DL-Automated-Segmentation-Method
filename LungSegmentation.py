8529fec1c3a144b4010f7de46f08e27ce19d50e0
isasilva3 <55702420+isasilva3@users.noreply.github.com>
2021-02-11 22:43:24 +0000

    Image format alteration

--- LungSegmentation.py
+++ LungSegmentation.py
@@ -26,11 +26,14 @@ Source: Catarina
 import argparse
 import logging
 import pathlib
+import sys
 
-from numpy import extract
+from numpy import extract, np
 
 from MONAI import monai
 from MONAI.monai.apps import extractall
+from MONAI.monai.data.image_reader import Image
+from MONAI.tests.utils import nib
 
 """## Setup imports"""
 
@@ -339,7 +342,53 @@ with torch.no_grad():
         plt.title(f"output {i}")
         plt.imshow(torch.argmax(val_outputs, dim=1).detach().cpu()[0, :, :, 80])
         plt.show()
-        fig3.savefig('image_label.tif')
+        fig3.savefig('image_label.tiff')
+
+def arg_parser():
+    parser = argparse.ArgumentParser(description='merge 2d tif images into a 3d image')
+    parser.add_argument('img_dir', type=str,
+                        help='//home//imoreira//JupyterExercise')
+    parser.add_argument('out_dir', type=str,
+                        help='//home//imoreira//JupyterExercise//FinalNiiImages')
+    parser.add_argument('-a', '--axis', type=int, default=2,
+                        help='axis on which to stack the 2d images')
+    return parser
+
+
+def split_filename(filepath):
+    path = os.path.dirname(filepath)
+    filename = os.path.basename(filepath)
+    base, ext = os.path.splitext(filename)
+    if ext == '.gz':
+        base, ext2 = os.path.splitext(base)
+        ext = ext2 + ext
+    return path, base, ext
+
+
+def main():
+    try:
+        args = arg_parser().parse_args()
+        img_dir = pathlib.Path(args.img_dir)
+        fns = sorted([str(fn) for fn in img_dir.glob('*.tif*')])
+        if not fns:
+            raise ValueError(f'img_dir ({args.img_dir}) does not contain any .tif or .tiff images.')
+        imgs = []
+        for fn in fns:
+            _, base, ext = split_filename(fn)
+            img = np.asarray(Image.open(fn)).astype(np.float32).squeeze()
+            if img.ndim != 2:
+                raise Exception(f'Only 2D data supported. File {base}{ext} has dimension {img.ndim}.')
+            imgs.append(img)
+        img = np.stack(imgs, axis=args.axis)
+        nib.Nifti1Image(img,None).to_filename(os.path.join(args.out_dir, f'{base}.nii.gz'))
+        return 0
+    except Exception as e:
+        print(e)
+        return 1
+
+
+if __name__ == "__main__":
+    sys.exit(main())
 
 
 '''
