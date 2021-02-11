0f94bbce6e02f77e28861d0e5dab2e791846d274
isasilva3 <55702420+isasilva3@users.noreply.github.com>
2021-02-11 22:20:02 +0000

    iamge format alteration

--- LungSegmentation.py
+++ LungSegmentation.py
@@ -23,10 +23,13 @@ Size: 10 3D volumes (8 Training + 2 Testing)
 Source: Catarina
 
 """
+import argparse
+import logging
 import pathlib
 
 from numpy import extract
 
+from MONAI import monai
 from MONAI.monai.apps import extractall
 
 """## Setup imports"""
@@ -94,13 +97,6 @@ The dataset comes from http://medicaldecathlon.com/.
 #var = (r'C:\Users\isasi\OneDrive\Desktop\Lungs.gz')
 #resource = "https://msd-for-monai.s3-us-west-2.amazonaws.com/Task09_Spleen.tar"
 md5 = "410d4a301da4e5b2f6f86ec3ddba524e"
-
-#filepath = r'C:\Users\isasi\OneDrive\Desktop\Lungs.zip'
-#filepath = '/home/imoreira/Data/Lungs.zip'
-#compressed_file = os.path.join(root_dir, "Lungs.zip")
-#data_dir = os.path.join(root_dir, "Lungs")
-#if not os.path.exists(data_dir):
-#    extractall(filepath, root_dir, hash_type='md5')
 
 root_dir = "//home//imoreira//Data"
 #root_dir = "C:\\Users\\isasi\\Downloads"
@@ -234,7 +230,7 @@ optimizer = torch.optim.Adam(model.parameters(), 1e-4)
 
 """## Execute a typical PyTorch training process"""
 
-epoch_num = 300
+epoch_num = 2
 val_interval = 2
 best_metric = -1
 best_metric_epoch = -1
@@ -343,7 +339,110 @@ with torch.no_grad():
         plt.title(f"output {i}")
         plt.imshow(torch.argmax(val_outputs, dim=1).detach().cpu()[0, :, :, 80])
         plt.show()
-        fig3.savefig('image_label.png')
+        fig3.savefig('image_label.nii')
+
+'''
+patch_size = (192, 192, 16)
+sw_batch_size, overlap = 2, 0.5
+inferer = monai.inferers.SlidingWindowInferer(
+    roi_size=patch_size,
+    sw_batch_size=sw_batch_size,
+    overlap=overlap,
+    mode="gaussian",
+    padding_mode="replicate",
+)
+
+
+ckpts = sorted(glob.glob(os.path.join(model_folder, "*.pt")))
+ckpt = ckpts[-1]
+for x in ckpts:
+    logging.info(f"available model file: {x}.")
+logging.info("----")
+logging.info(f"using {ckpt}.")
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+net = get_net().to(device)
+net.load_state_dict(torch.load(ckpt, map_location=device))
+net.eval()
+
+image_folder = os.path.abspath(data_folder)
+images = sorted(glob.glob(os.path.join(image_folder, "*_ct.nii.gz")))
+logging.info(f"infer: image ({len(images)}) folder: {data_folder}")
+infer_files = [{"image": img} for img in images]
+
+keys = ("image",)
+    infer_transforms = get_xforms("infer", keys)
+    infer_ds = monai.data.Dataset(data=infer_files, transform=infer_transforms)
+    infer_loader = monai.data.DataLoader(
+        infer_ds,
+        batch_size=1,  # image-level batch to the sliding window method, not the window-level batch
+        num_workers=2,
+        pin_memory=torch.cuda.is_available(),
+    )
+
+inferer = get_inferer()
+saver = monai.data.NiftiSaver(output_dir=prediction_folder, mode="nearest")
+with torch.no_grad():
+    for infer_data in infer_loader:
+        logging.info(f"segmenting {infer_data['image_meta_dict']['filename_or_obj']}")
+        preds = inferer(infer_data[keys[0]].to(device), net)
+        n = 1.0
+        for _ in range(4):
+            # test time augmentations
+            _img = RandGaussianNoised(keys[0], prob=1.0, std=0.01)(infer_data)[keys[0]]
+            pred = inferer(_img.to(device), net)
+            preds = preds + pred
+            n = n + 1.0
+            for dims in [[2], [3]]:
+                flip_pred = inferer(torch.flip(_img.to(device), dims=dims), net)
+                pred = torch.flip(flip_pred, dims=dims)
+                preds = preds + pred
+                n = n + 1.0
+        preds = preds / n
+        preds = (preds.argmax(dim=1, keepdims=True)).float()
+        saver.save_batch(preds, infer_data["image_meta_dict"])
+
+# copy the saved segmentations into the required folder structure for submission
+submission_dir = os.path.join(prediction_folder, "to_submit")
+if not os.path.exists(submission_dir):
+    os.makedirs(submission_dir)
+files = glob.glob(os.path.join(prediction_folder, "volume*", "*.nii.gz"))
+for f in files:
+    new_name = os.path.basename(f)
+    new_name = new_name[len("volume-covid19-A-0"):]
+    new_name = new_name[: -len("_ct_seg.nii.gz")] + ".nii.gz"
+    to_name = os.path.join(submission_dir, new_name)
+    shutil.copy(f, to_name)
+logging.info(f"predictions copied to {submission_dir}.")
+
+if __name__ == "__main__":
+"""
+Usage:
+    python run_net.py train --data_folder "COVID-19-20_v2/Train" # run the training pipeline
+    python run_net.py infer --data_folder "COVID-19-20_v2/Validation" # run the inference pipeline
+"""
+parser = argparse.ArgumentParser(description="Run a basic UNet segmentation baseline.")
+parser.add_argument(
+    "mode", metavar="mode", default="train", choices=("train", "infer"), type=str, help="mode of workflow"
+)
+parser.add_argument("--data_folder", default="", type=str, help="training data folder")
+parser.add_argument("--model_folder", default="runs", type=str, help="model folder")
+args = parser.parse_args()
+
+monai.config.print_config()
+monai.utils.set_determinism(seed=0)
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+if args.mode == "train":
+    data_folder = args.data_folder or os.path.join("COVID-19-20_v2", "Train")
+    train(data_folder=data_folder, model_folder=args.model_folder)
+elif args.mode == "infer":
+    data_folder = args.data_folder or os.path.join("COVID-19-20_v2", "Validation")
+    infer(data_folder=data_folder, model_folder=args.model_folder)
+else:
+    raise ValueError("Unknown mode.")
+
+'''
 
 """## Cleanup data directory
 
