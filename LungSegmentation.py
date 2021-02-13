c69e40ab44453f185729970b18436cde59942607
isasilva3 <55702420+isasilva3@users.noreply.github.com>
2021-02-13 20:48:03 +0000

    batch_size

--- LungSegmentation.py
+++ LungSegmentation.py
@@ -211,7 +211,7 @@ train_ds = CacheDataset(data=train_files, transform=train_transforms, cache_rate
 
 # use batch_size=2 to load images and use RandCropByPosNegLabeld
 # to generate 2 x 4 images for network training
-train_loader = DataLoader(train_ds, batch_size=1, shuffle=True, num_workers=0)
+train_loader = DataLoader(train_ds, batch_size=2, shuffle=True, num_workers=0)
 
 val_ds = CacheDataset(data=val_files, transform=val_transforms, cache_rate=1.0, num_workers=0)
 # val_ds = Dataset(data=val_files, transform=val_transforms)
