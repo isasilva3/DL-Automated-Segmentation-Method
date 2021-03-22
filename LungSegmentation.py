0bdef598111885d6c2950ab3f84705915c1b2ccc
isasilva3 <55702420+isasilva3@users.noreply.github.com>
2021-03-22 01:40:09 +0000

    test images

--- LungSegmentation.py
+++ LungSegmentation.py
@@ -226,7 +226,7 @@ val_ds = CacheDataset(data=val_files, transform=val_transforms, cache_rate=1.0, 
 # val_ds = Dataset(data=val_files, transform=val_transforms)
 val_loader = DataLoader(val_ds, batch_size=1, num_workers=0)
 
-test_ds = CacheDataset(data=test_files, cache_rate=1.0, num_workers=0)
+test_ds = CacheDataset(data=test_files, transform=train_transforms, cache_rate=1.0, num_workers=0)
 #test_ds = Dataset(data=test_files)
 test_loader = DataLoader(test_ds, batch_size=1, num_workers=0)
 
