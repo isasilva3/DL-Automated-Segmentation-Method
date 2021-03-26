d9eee7c190041251a434ac6da6904644514954e3
isasilva3 <55702420+isasilva3@users.noreply.github.com>
2021-03-26 22:12:12 +0000

    evaluation first try

---
+++ Evaluation.py
@@ -0,0 +1,20 @@
+
+import seg_metrics.seg_metrics as sg
+
+labels_dir = "//home//imoreira//Segmentations"
+
+
+labels_dicts = [{"image": image_name} for image_name in zip(labels_dir)]
+
+gdth_path = '//home//imoreira//Data//Lungs//labelsTr'
+
+pred_path = '//home//imoreira'
+
+csv_file = 'Metrics.csv'
+
+metrics = sg.write_metrics(labels=labels_dicts[1:],  # exclude background
+                  gdth_path=gdth_path,
+                  pred_path=pred_path,
+                  csv_file=csv_file)
+
+print(metrics)
