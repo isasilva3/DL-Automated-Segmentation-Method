7970db604a77f9603713ed3c977a3f18a6d71612
isasilva3 <55702420+isasilva3@users.noreply.github.com>
2021-03-27 16:15:38 +0000

    metrics

--- Evaluation.py
+++ Evaluation.py
@@ -8,11 +8,11 @@ labels_dir = '/home/imoreira/Segmentations/Pred'
 
 labels_dicts = [{"image": image_name} for image_name in zip(labels_dir)]
 
-gdth_path = '/home/imoreira/Data/Lungs/labelsTr/Labels'
+gdth_path = '//home//imoreira//Data//Lungs//labelsTr//Labels'
 
-pred_path = '/home/imoreira/Segmentations/Pred'
+pred_path = '//home//imoreira//Segmentations//Pred'
 
-csv_file = '/home/imoreira/Metrics.csv'
+csv_file = '//home//imoreira//Metrics.csv'
 
 metrics = sg.write_metrics(labels=labels_dicts[1:],  # exclude background
                   gdth_path=gdth_path,
@@ -24,11 +24,11 @@ print(metrics)
 
 labels = [0, 4, 5 ,6 ,7 , 8]
 
-gdth_file = '/home/imoreira/Data/Lungs/labelsTr/Labels/CNS044_lungs.nii.gz'
+gdth_file = '//home//imoreira//Data//Lungs//labelsTr//Labels//CNS044_lungs.nii.gz'
 
-pred_file = '/home/imoreira/Segmentations/Pred/CNS044_ct_seg.nii.gz'
+pred_file = '//home//imoreira//Segmentations//Pred//CNS044_ct_seg.nii.gz'
 
-csv_file = '/home/imoreira/Metrics.csv'
+csv_file = '//home//imoreira//Metrics.csv'
 
 metrics = sg.write_metrics(labels=labels[1:],  # exclude background if needed
                   gdth_path=gdth_file,
