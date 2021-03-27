9c53d0f3dce8881273b6043618c67ba7e475862c
isasilva3 <55702420+isasilva3@users.noreply.github.com>
2021-03-27 16:35:26 +0000

    metrics

--- Evaluation.py
+++ Evaluation.py
@@ -2,35 +2,21 @@
 import seg_metrics.seg_metrics as sg
 import SimpleITK as sitk
 
-'''
-labels_dir = '/home/imoreira/Segmentations/Pred'
+#labels_dir = '/home/imoreira/Segmentations/Pred'
 
+#labels_dicts = [{"image": image_name} for image_name in zip(labels_dir)]
 
-labels_dicts = [{"image": image_name} for image_name in zip(labels_dir)]
+labels = [0, 4, 5 ,6 ,7 , 8]
 
-gdth_path = '//home//imoreira//Data//Lungs//labelsTr//Labels'
+gdth_path = '/home/imoreira/Metrics/Labels'
 
-pred_path = '//home//imoreira//Segmentations//Pred'
+pred_path = '/home/imoreira/Metrics/Pred'
 
-csv_file = '//home//imoreira//Metrics.csv'
+csv_file = 'Metrics.csv'
 
-metrics = sg.write_metrics(labels=labels_dicts[1:],  # exclude background
+metrics = sg.write_metrics(labels=labels[1:],  # exclude background
                   gdth_path=gdth_path,
                   pred_path=pred_path,
                   csv_file=csv_file)
 
 print(metrics)
-'''
-
-labels = [0, 4, 5 ,6 ,7 , 8]
-
-gdth_file = '//home//imoreira//Data//Lungs//labelsTr//Labels//CNS044_lungs.nii.gz'
-
-pred_file = '//home//imoreira//Segmentations//Pred//CNS044_ct_seg.nii.gz'
-
-csv_file = '//home//imoreira//Metrics.csv'
-
-metrics = sg.write_metrics(labels=labels[1:],  # exclude background if needed
-                  gdth_path=gdth_file,
-                  pred_path=pred_file,
-                  csv_file=csv_file,)
