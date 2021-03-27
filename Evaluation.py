7c385d0fa6996b648c78383cb1ef77697e6da527
isasilva3 <55702420+isasilva3@users.noreply.github.com>
2021-03-27 16:07:59 +0000

    metrics

--- Evaluation.py
+++ Evaluation.py
@@ -2,7 +2,7 @@
 import seg_metrics.seg_metrics as sg
 import SimpleITK as sitk
 
-
+'''
 labels_dir = '/home/imoreira/Segmentations/Pred'
 
 
@@ -20,3 +20,17 @@ metrics = sg.write_metrics(labels=labels_dicts[1:],  # exclude background
                   csv_file=csv_file)
 
 print(metrics)
+'''
+
+labels = [0, 4, 5 ,6 ,7 , 8]
+
+gdth_file = '/home/imoreira/Data/Lungs/labelsTr/Labels/CNS044_lungs.nii.gz'
+
+pred_file = '/home/imoreira/Segmentations/Pred/CNS044_ct_seg.nii.gz'
+
+csv_file = '/home/imoreira/Metrics.csv'
+
+metrics = sg.write_metrics(labels=labels[1:],  # exclude background if needed
+                  gdth_path=gdth_file,
+                  pred_path=pred_file,
+                  csv_file=csv_file,)
