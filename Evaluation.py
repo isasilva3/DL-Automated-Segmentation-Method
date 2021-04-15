b97ff421b6700d0213c0f026fab392fb65bc86e6
isasilva3 <55702420+isasilva3@users.noreply.github.com>
2021-04-15 18:16:00 +0100

    permissions

--- Evaluation.py
+++ Evaluation.py
@@ -1,111 +1,127 @@
+import glob
+
 import torch
-
-from MONAI.monai.data import DataLoader
-from MONAI.monai.metrics import compute_hausdorff_distance
-from MONAI.monai.transforms import ToTensord, Compose, LoadImaged, AddChanneld
-
-import metrics as metrics
+import os
 import numpy as np
 import nibabel as nib
-from scipy.spatial.distance import directed_hausdorff, euclidean, cdist
-import seg_metrics.seg_metrics as sg
 
-from monai.utils import optional_import
+from surface_distance import metrics, compute_surface_distances, compute_robust_hausdorff, \
+    compute_average_surface_distance
 
 
-binary_dilation, _ = optional_import("scipy.ndimage.morphology", name="binary_dilation")
-directed_hausdorff, _ = optional_import("scipy.spatial.distance", name="directed_hausdorff")
-
-#gdth_path=nib.load('C:\\Users\\isasi\\Downloads\\Metrics\\Labels\\CNS054_liver.nii.gz')
-#pred_path=nib.load('C:\\Users\\isasi\\Downloads\\Metrics\\Pred\\CNS054_ct_seg_liver.nii.gz')
-
-gdth_path=nib.load('/home/imoreira/Metrics/Labels/CNS044_lungs.nii.gz')
-pred_path=nib.load('/home/imoreira/Metrics/Pred/CNS044_ct_seg_lungs.nii.gz')
-
-gdth_data = gdth_path.get_fdata()
-pred_data = pred_path.get_fdata()
-
-gdth_data_arr = np.asarray(gdth_data).astype(np.bool)
-pred_data_arr = np.asarray(pred_data).astype(np.bool)
 
 '''
-Compute the dice score between two input images or volumes. Note that we use a smoothing factor of 1.
-:param im1: Image 1
-:param im2: Image 2
-:return: Dice score
+ITERATING FORM
+
+data_path = 'path_to_your_data_folder'
+_, _, filenames = next(os.walk(data_path))
+ct_masks_list = []
+[ct_masks_list.append(f) for f in filenames if (('CNS0' in f)]
+
+for mask_file in ct_masks_list:
+ (here you put your code to read in the mask file and corresponding ground truth mask file, and then do your calculations)
+ 
 '''
 
-#pred_data_arr = np.rollaxis(pred_data_arr,3,1).reshape(1,-1,1)
-
-if gdth_data_arr.shape != pred_data_arr.shape:
-
-    pred_data_arr.shape = gdth_data_arr.shape
-
-    print("pred_shape is:", pred_data_arr.shape)
-    print("gdth_shape is:", gdth_data_arr.shape)
-
-'''
-# Compute Dice coefficient
-intersection = np.logical_and(gdth_data_arr, pred_data_arr)
-
-dice_score = (2 * intersection.sum() + 1) / (gdth_data_arr.sum() + pred_data_arr.sum() + 1)
 
 
-#JACCARD INDEX
+#gdth_path="C:\\Users\\isasi\\Downloads\\Metrics\\Labels\\"
+#pred_path='C:\\Users\\isasi\\Downloads\\Metrics\\Pred\\'
+#mas_path='C:\\Users\\isasi\\Downloads\\Metrics\\MAS'
 
-intersection = np.logical_and(gdth_data_arr, pred_data_arr)
+gdth_path="/home/imoreira/Metrics/Labels/"
+pred_path='/home/imoreira/Metrics/Labels/'
 
-union = np.logical_or(gdth_data_arr, pred_data_arr)
+_, _, filenames_gd = next(os.walk(gdth_path))
+_, _, filenames_pred = next(os.walk(pred_path))
+#_, _, filenames_mas = next(os.walk(mas_path))
 
-jc = intersection.sum() / float(union.sum())
+gdth_list = []
+pred_list = []
+#mas_list = []
 
-#HAUSDORFF DISTANCE
-'''
-if not (np.any(gdth_data_arr) and np.any(pred_data_arr)):
-    raise ValueError(f"Labelfields should have at least 1 voxel containing the desired labelfield")
+for f in filenames_gd:
+    gdth_list.append(f)
 
-# Do binary dilation and use XOR to get edges
-edges_1 = binary_dilation(gdth_data_arr) ^ gdth_data_arr
-edges_2 = binary_dilation(pred_data_arr) ^ pred_data_arr
+for i in filenames_pred:
+    pred_list.append(i)
 
-# Extract coordinates of these edge points
-coords_1 = np.argwhere(edges_1)
-coords_2 = np.argwhere(edges_2)
+#for j in filenames_mas:
+#    mas_list.append(j)
 
-# Get (potentially directed) Hausdorff distance
-#if directed:
-#hd = float(directed_hausdorff(coords_1, coords_2)[0])
-
-#else:
-#    hd = float(max(directed_hausdorff(coords_1, coords_2)[0], directed_hausdorff(coords_2, coords_1)[0]))
-
-min_euc_list_u = []
-min_euc_list_v = []
-
-for i in range(coords_1.shape[0]):
-    euc_list = []
-    for j in range(coords_2.shape[0]):
-        euc = euclidean(coords_1[i, :], coords_2[j, :])
-        euc_list.append(euc)
-    min_euc_list_u.append(min(euc_list))
-
-for i in range(coords_2.shape[0]):
-    euc_list = []
-    for j in range(coords_1.shape[0]):
-        euc = euclidean(coords_2[i, :], coords_1[j, :])
-        euc_list.append(euc)
-    min_euc_list_v.append(min(euc_list))
-
-hd = max(np.array(min_euc_list_u).mean(), np.array(min_euc_list_v).mean())
-
-#AVARAGE DISTANCE
-
-#ad = cdist(gdth_data_arr, pred_data_arr)
+for f in gdth_list:
+    #print(f)
+    gdth_data = nib.load(f)
+    gdth_data = gdth_data.get_fdata()
+    gdth_arr = np.asarray(gdth_data).astype(np.bool)
 
 
-#print("The dice score is:", dice_score)
-#print("The jaccard index is:", jc)
-print("The hausdorff distance is:", hd)
-#print("The average distance is:", ad)
+#mas_path=nib.load('C:\\Users\\isasi\\Downloads\\Metrics\\MAS\\CNS044_bladder_MAS_majority.nii.gz')
+
+#gdth_path=nib.load('/home/imoreira/Metrics/Labels/CNS044_lungs.nii.gz')
+#pred_path=nib.load('/home/imoreira/Metrics/Pred/CNS044_ct_seg_lungs.nii.gz')
+#mas_path=nib.load('/home/imoreira/Metrics/MAS/CNS044_bladder_MAS_majority.nii.gz')
+
+
+
+    #gdth_data = gdth_path.get_fdata()
+    #pred_data = pred_path.get_fdata()
+    #mas_data = mas_path.get_fdata()
+
+    #gdth_data_arr = np.asarray(gdth_name).astype(np.bool)
+    #pred_data_arr = np.asarray(pred_name.astype(np.bool)
+    #mas_data_arr = np.asarray(mas_data).astype(np.bool)
+
+    #pred_data_arr = np.squeeze(pred_data_arr)
+
+    #print("pred_shape is:", pred_data_arr.shape)
+    print("gdth_shape is:", gdth_arr.shape)
+
+    #Makes sure both arrays have the same size
+
+    if gdth_data_arr.shape != pred_data_arr.shape:
+
+        print("The arrays have different shapes.")
+
+    else:
+        # Compute Dice coefficient
+        intersection = np.logical_and(gdth_data_arr, pred_data_arr)
+
+        dice_score = (2 * intersection.sum()) / (gdth_data_arr.sum() + pred_data_arr.sum() + 0.001)
+
+        #Nan Condition
+
+        if dice_score == 0:
+            dice_score = "Nan"
+
+        else:
+            dice_score = dice_score
+
+
+        #JACCARD INDEX
+
+        intersection = np.logical_and(gdth_data_arr, pred_data_arr)
+
+        union = np.logical_or(gdth_data_arr, pred_data_arr)
+
+        jc = intersection.sum() / float(union.sum())
+
+        #HAUSDORFF DISTANCE
+
+        sx, sy, sz = gdth_path.header.get_zooms()
+
+        surface_distances = compute_surface_distances(gdth_data_arr, pred_data_arr, (sx, sy, sz))
+
+        hd = compute_robust_hausdorff(surface_distances, percent=100)
+
+        #AVERAGE DISTANCE
+
+        ad = compute_average_surface_distance(surface_distances)
+
+        print("The dice score is:", dice_score)
+        print("The jaccard index is:", jc)
+        print("The surface distance are:", surface_distances)
+        print("The hausdorff distance is:", hd, "mm")
+        print("The gdth_surface-pred_surface average distance is:", ad[0], "mm", "and the pred_surface-gdth_surface average distance is:", ad[1], "mm")
 
 
