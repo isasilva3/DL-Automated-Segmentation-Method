d97256ef6e3e2e9b682f17ede4846dca732bf56d
isasilva3 <55702420+isasilva3@users.noreply.github.com>
2021-06-26 16:55:28 +0100

    Bladder noised, lungs smooth, prob 0.5

--- BladderSegmentation.py
+++ BladderSegmentation.py
@@ -155,13 +155,13 @@ train_transforms = Compose(
         #    padding_mode="zeros",
         #    as_tensor_output=False
         #),
-        #RandGaussianNoised(
-        #    keys=["image"],
-        #    prob=0.1,
-        #    mean=0.0,
-        #    std=0.1
-        # allow_missing_keys=False
-        #),
+        RandGaussianNoised(
+            keys=["image"],
+            prob=0.5,
+            mean=0.0,
+            std=0.1
+         #allow_missing_keys=False
+        ),
        #RandScaleIntensityd(
        #    keys=["image"],
        #    factors=0.05,  # this is 10%, try 5%
