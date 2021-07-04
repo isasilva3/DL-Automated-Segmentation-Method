36c0ef720fe313ac18b2f01df3cfabc672c5a802
isasilva3 <55702420+isasilva3@users.noreply.github.com>
2021-07-04 02:41:11 +0100

    agora deu

--- LiverSegmentation.py
+++ LiverSegmentation.py
@@ -232,7 +232,7 @@ test_transforms = Compose(
 """## Check transforms in DataLoader"""
 
 check_ds = Dataset(data=val_files, transform=val_transforms)
-check_loader = DataLoader(check_ds, batch_size=2)
+check_loader = DataLoader(check_ds, batch_size=1)
 check_data = first(check_loader)
 image, label = (check_data["image"][0][0], check_data["label"][0][0])
 print(f"image shape: {image.shape}, label shape: {label.shape}")
@@ -257,23 +257,23 @@ And set `num_workers` to enable multi-threads during caching.
 If want to to try the regular Dataset, just change to use the commented code below.
 """
 
-train_ds = CacheDataset(data=train_files, transform=train_transforms, cache_rate=1.0, num_workers=1)
+train_ds = CacheDataset(data=train_files, transform=train_transforms, cache_rate=1.0, num_workers=2)
 # train_ds = monai.data.Dataset(data=train_files, transform=train_transforms)
 
 # use batch_size=2 to load images and use RandCropByPosNegLabeld
 # to generate 2 x 4 images for network training
-train_loader = DataLoader(train_ds, batch_size=1, shuffle=True, num_workers=1)
+train_loader = DataLoader(train_ds, batch_size=1, shuffle=True, num_workers=2)
 
-train_inf_ds = CacheDataset(data=train_files, transform=train_inf_transforms, cache_rate=1.0, num_workers=1)
-train_inf_loader = DataLoader(train_inf_ds, batch_size=1, num_workers=1)
+train_inf_ds = CacheDataset(data=train_files, transform=train_inf_transforms, cache_rate=1.0, num_workers=2)
+train_inf_loader = DataLoader(train_inf_ds, batch_size=1, num_workers=2)
 
-val_ds = CacheDataset(data=val_files, transform=val_transforms, cache_rate=1.0, num_workers=1)
+val_ds = CacheDataset(data=val_files, transform=val_transforms, cache_rate=1.0, num_workers=2)
 # val_ds = Dataset(data=val_files, transform=val_transforms)
-val_loader = DataLoader(val_ds, batch_size=1, num_workers=1)
+val_loader = DataLoader(val_ds, batch_size=1, num_workers=2)
 
-test_ds = CacheDataset(data=test_files, transform=test_transforms, cache_rate=1.0, num_workers=1)
+test_ds = CacheDataset(data=test_files, transform=test_transforms, cache_rate=1.0, num_workers=2)
 #test_ds = Dataset(data=test_files)
-test_loader = DataLoader(test_ds, batch_size=1, num_workers=1)
+test_loader = DataLoader(test_ds, batch_size=1, num_workers=2)
 
 
 """## Create Model, Loss, Optimizer"""
