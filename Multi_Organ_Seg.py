3a2d1998ee8352d515c395a304c9aa852f520d33
isasilva3 <55702420+isasilva3@users.noreply.github.com>
2021-09-29 14:24:08 +0100

    Add channels

--- Multi_Organ_Seg.py
+++ Multi_Organ_Seg.py
@@ -83,12 +83,12 @@ Label 5: Pancreas
 train_transforms = Compose(
     [
         LoadImaged(keys=["image", "label"]),
-        AddChanneld(keys=["image", "label"]),
-        AddChanneld(keys=["image", "label"]),
-        AddChanneld(keys=["image", "label"]),
-        AddChanneld(keys=["image", "label"]),
-        AddChanneld(keys=["image", "label"]),
-        AddChanneld(keys=["image", "label"]),
+        AddChanneld(keys=["label"]),
+        AddChanneld(keys=["label"]),
+        AddChanneld(keys=["label"]),
+        AddChanneld(keys=["label"]),
+        AddChanneld(keys=["label"]),
+        AddChanneld(keys=["label"]),
         Spacingd(keys=["image", "label"], pixdim=(1.5, 1.5, 2.0), mode=("bilinear", "nearest")),
         Orientationd(keys=["image", "label"], axcodes="RAS"),
         ScaleIntensityRanged(
