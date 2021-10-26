da9541e05b87364e0e57c1e62af57f3f489e8884
isasilva3 <55702420+isasilva3@users.noreply.github.com>
2021-10-26 01:24:27 +0100

    with bool

--- Inference.py
+++ Inference.py
@@ -167,99 +167,6 @@ Label 4: Lungs
 Label 5: Pancreas
 '''
 
-train_transforms = Compose(
-    [
-        LoadImaged(keys=["image", "label"]),
-        AddChanneld(keys=["image", "label"]),
-        Spacingd(keys=["image", "label"], pixdim=(1.5, 1.5, 2.0), mode=("bilinear", "nearest")),
-        Orientationd(keys=["image", "label"], axcodes="RAS"),
-        ScaleIntensityRanged(
-            keys=["image"], a_min=-1000, a_max=300, b_min=0.0, b_max=1.0, clip=True,
-        ),
-        #CropForegroundd(keys=["image", "label"], source_key="image"),
-        RandCropByPosNegLabeld(
-            keys=["image", "label"],
-            label_key="label",
-            spatial_size=(96, 96, 96),
-            pos=1,
-            neg=1,
-            num_samples=4,
-            image_key="image",
-            image_threshold=0,
-        ),
-        #Rand3DElasticd(
-        #    keys=["image", "label"],
-        #    sigma_range=(0, 1),
-        #    magnitude_range=(0, 1),
-        #    spatial_size=None,
-        #    prob=0.5,
-        #    rotate_range=(-math.pi / 36, math.pi / 36),  # -15, 15 / -5, 5
-        #    shear_range=None,
-        #    translate_range=None,
-        #    scale_range=None,
-        #    mode=("bilinear", "nearest"),
-        #    padding_mode="zeros",
-        #   as_tensor_output=False
-        #),
-        #RandGaussianNoised(
-        #    keys=["image"],
-        #    prob=0.5,
-        #   mean=0.0,
-        #    std=0.1
-         #allow_missing_keys=False
-        #),
-       #RandScaleIntensityd(
-       #    keys=["image"],
-       #    factors=0.05,  # this is 10%, try 5%
-       #    prob=0.5
-       #),
-       #RandGaussianSmoothd(
-       #    keys=["image"],
-       #    sigma_x=(0.25, 1.5),
-       #    sigma_y=(0.25, 1.5),
-       #    sigma_z=(0.25, 1.5),
-       #   prob=0.5,
-       #   approx='erf'
-            # allow_missing_keys=False
-       #),
-       #RandAdjustContrastd(
-       #    keys=["image"],
-       #    prob=0.5,
-       #    gamma=(0.9, 1.1)
-           #allow_missing_keys=False
-       #),
-        # user can also add other random transforms
-        # RandAffined(keys=['image', 'label'], mode=('bilinear', 'nearest'), prob=1.0, spatial_size=(96, 96, 96),
-        #             rotate_range=(0, 0, np.pi/15), scale_range=(0.1, 0.1, 0.1)),
-
-       ToTensord(keys=["image", "label"]),
-    ]
-)
-#train_inf_transforms = Compose(
-#    [
-#        LoadImaged(keys=["image", "label"]),
-#        AddChanneld(keys=["image", "label"]),
-#        Spacingd(keys=["image", "label"], pixdim=(1.5, 1.5, 2.0), mode=("bilinear", "nearest")),
-#        Orientationd(keys=["image", "label"], axcodes="RAS"),
-#        ScaleIntensityRanged(
-#            keys=["image"], a_min=-350, a_max=50, b_min=0.0, b_max=1.0, clip=True,
-#        ),
-#        ToTensord(keys=["image", "label"]),
-#    ]
-#)
-val_transforms = Compose(
-    [
-        LoadImaged(keys=["image", "label"]),
-        AddChanneld(keys=["image", "label"]),
-        Spacingd(keys=["image", "label"], pixdim=(1.5, 1.5, 2.0), mode=("bilinear", "nearest")),
-        Orientationd(keys=["image", "label"], axcodes="RAS"),
-        ScaleIntensityRanged(
-            keys=["image"], a_min=-1000, a_max=300, b_min=0.0, b_max=1.0, clip=True,
-        ),
-        #CropForegroundd(keys=["image", "label"], source_key="image"),
-        ToTensord(keys=["image", "label"]),
-    ]
-)
 test_transforms = Compose(
     [
         LoadImaged(keys=["image", "label"]),
@@ -274,44 +181,9 @@ test_transforms = Compose(
     ]
 )
 
-"""## Check transforms in DataLoader"""
-
-check_ds = Dataset(data=val_files, transform=val_transforms)
-check_loader = DataLoader(check_ds, batch_size=1)
-check_data = first(check_loader)
-image, label = (check_data["image"][0][0], check_data["label"][0][0])
-print(f"image shape: {image.shape}, label shape: {label.shape}")
-# plot the slice [:, :, 80]
-fig = plt.figure("check", (12, 6)) #figure size
-plt.subplot(1, 2, 1)
-plt.title("image")
-plt.imshow(image[:, :, 80], cmap="gray")
-plt.subplot(1, 2, 2)
-plt.title("label")
-plt.imshow(label[:, :, 80])
-#plt.show()
-#fig.savefig('my_figure.png')
-
-
-train_ds = CacheDataset(data=train_files, transform=train_transforms, cache_rate=1.0, num_workers=2)
-# train_ds = monai.data.Dataset(data=train_files, transform=train_transforms)
-
-# use batch_size=2 to load images and use RandCropByPosNegLabeld
-# to generate 2 x 4 images for network training
-train_loader = DataLoader(train_ds, batch_size=1, shuffle=True, num_workers=2)
-
-
-#train_inf_ds = CacheDataset(data=train_files, transform=train_inf_transforms, cache_rate=1.0, num_workers=2)
-#train_inf_loader = DataLoader(train_inf_ds, batch_size=1, num_workers=2)
-
-val_ds = CacheDataset(data=val_files, transform=val_transforms, cache_rate=1.0, num_workers=2)
-# val_ds = Dataset(data=val_files, transform=val_transforms)
-val_loader = DataLoader(val_ds, batch_size=1, num_workers=2)
-
 test_ds = CacheDataset(data=test_files, transform=test_transforms, cache_rate=1.0, num_workers=2)
 #test_ds = Dataset(data=test_files)
 test_loader = DataLoader(test_ds, batch_size=1, num_workers=2)
-
 
 """## Create Model, Loss, Optimizer"""
 
