683872739a228a1884bab779019027657ca4a602
isasilva3 <55702420+isasilva3@users.noreply.github.com>
2021-11-24 18:04:58 +0000

    val loader batch size 1

--- Multi_Organ_Seg.py
+++ Multi_Organ_Seg.py
@@ -224,7 +224,7 @@ train_loader = DataLoader(train_ds, batch_size=4, shuffle=True, num_workers=0)
 
 val_ds = CacheDataset(data=val_files, transform=val_transforms, cache_rate=1.0, num_workers=0)
 # val_ds = Dataset(data=val_files, transform=val_transforms)
-val_loader = DataLoader(val_ds, batch_size=2, num_workers=0)
+val_loader = DataLoader(val_ds, batch_size=1, num_workers=0)
 
 test_ds = CacheDataset(data=test_files, transform=test_transforms, cache_rate=1.0, num_workers=0)
 #test_ds = Dataset(data=test_files)
