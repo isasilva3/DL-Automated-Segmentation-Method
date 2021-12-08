7b5849f869244fc9eae40e2b46a1e97799479bda
isasilva3 <55702420+isasilva3@users.noreply.github.com>
2021-12-08 16:57:18 +0000

    val_outputs.size()

--- PancreasSegmentation.py
+++ PancreasSegmentation.py
@@ -425,11 +425,12 @@ with torch.no_grad():
     for i, test_data in enumerate(test_loader):
         test_images = test_data["image"].to(device)
         roi_size = (96, 96, 96)
-        sw_batch_size = 2
+        sw_batch_size = 4
         val_outputs = sliding_window_inference(
-            test_images, roi_size, sw_batch_size, model
+            test_images, roi_size, sw_batch_size, model, overlap=0.8
         )
         val_outputs = val_outputs.argmax(dim=1, keepdim=True)
+        print(val_outputs.size())
         val_outputs = largest(val_outputs)
 
         val_outputs = val_outputs.cpu().clone().numpy()
