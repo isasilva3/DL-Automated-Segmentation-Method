72a4ae586ae497c90831ecff7155cf6fc851077d
isasilva3 <55702420+isasilva3@users.noreply.github.com>
2021-12-10 22:18:27 +0000

    Pancreas squeeze

--- PancreasSegmentation.py
+++ PancreasSegmentation.py
@@ -436,7 +436,7 @@ with torch.no_grad():
             test_images, roi_size, sw_batch_size, model, overlap=0.8
         )
 
-        val_outputs = np.squeeze(val_outputs)
+        val_outputs = np.squeeze(val_outputs, axis=0)
 
         val_outputs = torch.tensor(val_outputs)
 
