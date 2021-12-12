29f5e8ef923923a4f0de28eddf2307d33311d9b5
isasilva3 <55702420+isasilva3@users.noreply.github.com>
2021-12-12 23:06:47 +0000

    Pancreas inference and brain

--- Inference.py
+++ Inference.py
@@ -97,7 +97,7 @@ test_transforms = Compose(
 
 test_ds = CacheDataset(data=test_files, transform=test_transforms, cache_rate=1.0, num_workers=2)
 #test_ds = Dataset(data=test_files)
-test_loader = DataLoader(test_ds, batch_size=1, num_workers=2)
+test_loader = DataLoader(test_ds, batch_size=4, num_workers=2)
 
 """## Create Model, Loss, Optimizer"""
 
@@ -137,7 +137,7 @@ with torch.no_grad():
                        )
     for test_data in test_loader:
         test_images = test_data["image"].to(device)
-        roi_size = (160, 160, 160)
+        roi_size = (96, 96, 96)
         sw_batch_size = 4
         val_outputs = sliding_window_inference(
             test_images, roi_size, sw_batch_size, model, overlap=0.8
@@ -148,7 +148,7 @@ with torch.no_grad():
         val_outputs = val_outputs.argmax(dim=1, keepdim=True)
         val_outputs = largest(val_outputs)
         val_outputs = val_outputs.cpu().clone().numpy()
-        val_outputs = val_outputs.astype(np.bool)
+        val_outputs = val_outputs.astype(np.int)
 
         saver.save_batch(val_outputs, test_data["image_meta_dict"])
 
