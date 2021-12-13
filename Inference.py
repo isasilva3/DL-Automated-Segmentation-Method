6581b41e02bed1dd6be2eb8e235b894cb7330108
isasilva3 <55702420+isasilva3@users.noreply.github.com>
2021-12-13 14:30:34 +0000

    pancreas inference

--- Inference.py
+++ Inference.py
@@ -135,18 +135,21 @@ with torch.no_grad():
                        mode="nearest",
                        padding_mode="zeros"
                        )
-    for test_data in test_loader:
+    for i, test_data in enumerate(test_loader):
         test_images = test_data["image"].to(device)
         roi_size = (96, 96, 96)
-        sw_batch_size = 1
+        sw_batch_size = 4
+
         val_outputs = sliding_window_inference(
             test_images, roi_size, sw_batch_size, model, overlap=0.8
         )
 
-        val_outputs = torch.squeeze(val_outputs, dim=1)
+        # val_outputs = torch.squeeze(val_outputs, dim=1)
 
-        #val_outputs = val_outputs.argmax(dim=0, keepdim=True)
-        val_outputs = largest(val_outputs)
+        val_outputs = val_outputs.argmax(dim=1, keepdim=True)
+
+        # val_outputs = largest(val_outputs)
+
         val_outputs = val_outputs.cpu().clone().numpy()
         val_outputs = val_outputs.astype(np.bool)
 
