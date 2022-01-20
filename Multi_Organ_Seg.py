9d952a095bdfa9f2ceaff2d01c2b80a1f7f0375d
isasilva3 <55702420+isasilva3@users.noreply.github.com>
2022-01-20 01:22:22 +0000

    800 epochs, diceCE, with kidneys

--- Multi_Organ_Seg.py
+++ Multi_Organ_Seg.py
@@ -249,8 +249,8 @@ model = UNet(
 ).to(device)
 
 
-loss_function = DiceLoss(include_background=True, to_onehot_y=True, softmax=True)
-#loss_function = DiceCELoss(include_background=True, to_onehot_y=True, softmax=True, lambda_dice=0.5, lambda_ce=0.5)
+#loss_function = DiceLoss(include_background=True, to_onehot_y=True, softmax=True)
+loss_function = DiceCELoss(include_background=True, to_onehot_y=True, softmax=True, lambda_dice=0.5, lambda_ce=0.5)
 optimizer = torch.optim.Adam(model.parameters(), 1e-3)
 dice_metric = DiceMetric(include_background=False, reduction="mean")
 scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'max', factor=0.5) ##
