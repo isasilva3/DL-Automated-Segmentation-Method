2843635e97aa35972c1ad073815b619203fa020c
isasilva3 <55702420+isasilva3@users.noreply.github.com>
2022-01-28 15:30:33 +0000

    train inf loader batch size 1

--- Multi_Organ_Seg.py
+++ Multi_Organ_Seg.py
@@ -222,7 +222,7 @@ train_loader = DataLoader(train_ds, batch_size=4, shuffle=True, num_workers=0)
 
 
 train_inf_ds = CacheDataset(data=train_files, transform=train_inf_transforms, cache_rate=1.0, num_workers=0)
-train_inf_loader = DataLoader(train_inf_ds, batch_size=4, num_workers=0)
+train_inf_loader = DataLoader(train_inf_ds, batch_size=1, num_workers=0)
 
 val_ds = CacheDataset(data=val_files, transform=val_transforms, cache_rate=1.0, num_workers=0)
 # val_ds = Dataset(data=val_files, transform=val_transforms)
