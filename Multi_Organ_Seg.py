4a89e464a6162fd4037adb7ec396c58e0f10ad4f
isasilva3 <55702420+isasilva3@users.noreply.github.com>
2022-02-12 00:18:50 +0000

    Trainf inferences MO, -1000, 300

--- Multi_Organ_Seg.py
+++ Multi_Organ_Seg.py
@@ -221,8 +221,8 @@ train_ds = CacheDataset(data=train_files, transform=train_transforms, cache_rate
 train_loader = DataLoader(train_ds, batch_size=4, shuffle=True, num_workers=0)
 
 
-# train_inf_ds = CacheDataset(data=train_files, transform=train_inf_transforms, cache_rate=1.0, num_workers=0)
-# train_inf_loader = DataLoader(train_inf_ds, batch_size=1, num_workers=0)
+train_inf_ds = CacheDataset(data=train_files, transform=train_inf_transforms, cache_rate=1.0, num_workers=0)
+train_inf_loader = DataLoader(train_inf_ds, batch_size=1, num_workers=0)
 
 val_ds = CacheDataset(data=val_files, transform=val_transforms, cache_rate=1.0, num_workers=0)
 # val_ds = Dataset(data=val_files, transform=val_transforms)
@@ -413,14 +413,14 @@ with torch.no_grad():
                        mode="nearest",
                        padding_mode="zeros"
                        )
-    for i, test_data in enumerate(test_loader):
+    for i, train_inf_data in enumerate(train_inf_loader):
     #for test_data in test_loader:
-        test_images = test_data["image"].to(device)
+        train_inf_images = train_inf_data["image"].to(device)
         roi_size = (96, 96, 96)
         sw_batch_size = 4
 
         val_outputs = sliding_window_inference(
-            test_images, roi_size, sw_batch_size, model, overlap=0.8
+            train_inf_images, roi_size, sw_batch_size, model, overlap=0.8
         )
         val_outputs = val_outputs.argmax(dim=1, keepdim=True)
         #val_outputs = val_outputs.squeeze(dim=0).cpu().clone().numpy()
@@ -432,4 +432,4 @@ with torch.no_grad():
         #val_outputs = torch.argmax(val_outputs, dim=1)
         #val_outputs = val_outputs.squeeze(dim=0).cpu().data.numpy()
 
-        saver.save_batch(val_outputs, test_data["image_meta_dict"])
+        saver.save_batch(val_outputs, train_inf_data["image_meta_dict"])
