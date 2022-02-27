81b0b907e0472093d4581d7a6b7b256c057fefaa
isasilva3 <55702420+isasilva3@users.noreply.github.com>
2022-02-27 23:41:46 +0000

    All train

--- PancreasSegmentation.py
+++ PancreasSegmentation.py
@@ -428,13 +428,13 @@ with torch.no_grad():
                        padding_mode="zeros"
                        )
 
-    for test_data in test_loader:
-        test_images = test_data["image"].to(device)
+    for train_inf_data in train_inf_loader:
+        train_inf_images = train_inf_data["image"].to(device)
         roi_size = (96, 96, 96)
         sw_batch_size = 4
 
         val_outputs = sliding_window_inference(
-            test_images, roi_size, sw_batch_size, model, overlap=0.8
+            train_inf_images, roi_size, sw_batch_size, model, overlap=0.8
         )
 
         # val_outputs = torch.squeeze(val_outputs, dim=1)
@@ -446,4 +446,4 @@ with torch.no_grad():
         val_outputs = val_outputs.cpu().clone().numpy()
         val_outputs = val_outputs.astype(np.bool)
 
-        saver.save_batch(val_outputs, test_data["image_meta_dict"])
+        saver.save_batch(val_outputs, train_inf_data["image_meta_dict"])
