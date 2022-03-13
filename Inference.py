d47adcec67e29e04c4872e72f160b870d045b67a
isasilva3 <55702420+isasilva3@users.noreply.github.com>
2022-03-13 19:48:00 +0000

    kidneys inference

--- Inference.py
+++ Inference.py
@@ -203,8 +203,8 @@ test_transforms = Compose(
 check_ds = Dataset(data=val_files, transform=val_transforms)
 check_loader = DataLoader(check_ds, batch_size=1)
 check_data = first(check_loader)
-image, label = (check_data["image"][0][0], check_data["label"][0][0])
-print(f"image shape: {image.shape}, label shape: {label.shape}")
+#image, label = (check_data["image"][0][0], check_data["label"][0][0])
+#print(f"image shape: {image.shape}, label shape: {label.shape}")
 # plot the slice [:, :, 80]
 #fig = plt.figure("check", (12, 6)) #figure size
 #plt.subplot(1, 2, 1)
